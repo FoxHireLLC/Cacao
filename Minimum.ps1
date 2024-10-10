fff8a415a211c49984b83d39588d370b2af7c5b6
LemmingRyan <ryanwilliams1074@gmail.com>
2024-10-10 15:57:18 -0400

    Limiting the minimum installs.

--- Minimum.ps1
+++ Minimum.ps1
@@ -21,10 +21,12 @@ function executeScript {
     iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
 }
 
+#--- Setting up Windows ---
+executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
-executeScript "Browsers.ps1";
-executeScript "WindowsTools.ps1";
-executeScript "Foxhire.ps1";
+choco install -y googlechrome
+choco install -y openvpn
+choco install -y slack
 
 Enable-UAC
 Enable-MicrosoftUpdate
