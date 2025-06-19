675b1de35c2f686e323152ca9fc5f863f2c938fb
Ryan Williams <ryanwilliams1074@gmail.com>
2025-06-19 16:28:35 -0400

    Update CommonDevTools.ps1

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -8,16 +8,16 @@ choco install -y gh
 choco install -y github-desktop
 choco install -y kdiff3
 choco install -y SourceTree
-# choco install -y jetbrainstoolbox
-# choco install -y vscode
+choco install -y jetbrainstoolbox
+choco install -y vscode
 choco install -y notepadplusplus
 choco install -y python
-# choco install -y nodejs
+choco install -y nodejs
 choco install -y sysinternals
 choco install -y winscp
 choco install -y rapidee
-# choco install -y putty
-# choco install -y sql-server-management-studio
+choco install -y putty
+choco install -y sql-server-management-studio
 choco install -y linqpad
 choco install -y postman
 choco install -y sql-server-2022
