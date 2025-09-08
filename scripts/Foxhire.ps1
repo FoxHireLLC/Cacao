d12897b0c528a0af29d5746bc2ede3339d3a2dd4
Ryan Williams <ryanwilliams1074@gmail.com>
2025-09-08 13:25:24 -0400

    Update Foxhire.ps1

--- scripts/Foxhire.ps1
+++ scripts/Foxhire.ps1
@@ -4,3 +4,5 @@
 choco install -y openvpn
 choco install -y cyberduck
 choco install -y slack
+
+winget install "Nextiva.NextivaONE" --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --force
